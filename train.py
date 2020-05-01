dd8ed5a996d77d976f386b2fc0f61b41d3efab11
hansmu <hans.miikael@gmail.com>
2020-05-01 12:20:50 +0300

    Add additional multiplier to reduce value faster at steps

--- train.py
+++ train.py
@@ -150,9 +150,8 @@ def train():
 
     # Scheduler https://arxiv.org/pdf/1812.01187.pdf
     def lropt(x):
-        print('Epoch?', x)
-        print((((1 + math.cos(x * math.pi / epochs)) / 2) ** 1.0) * 0.95 + 0.05)
-        return (((1 + math.cos(x * math.pi / epochs)) / 2) ** 1.0) * 0.95 + 0.05  # cosine
+        additional_multiplier = 0.6 ** (x // 15)
+        return ((((1 + math.cos(x * math.pi / epochs)) / 2) ** 1.0) * 0.95 + 0.05) * additional_multiplier  # cosine
 
     lf = lropt  # cosine
     scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lf)
